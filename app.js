fe177b2b2ab1523b61ebabe45000581a7061a66c
keerthi D <keerthidev36@gmail.com>
2022-02-26 15:54:25 +0530

    newchange

--- app.js
+++ app.js
@@ -1,72 +1,53 @@
 const express= require('express');
-const nodemailer= require('nodemailer');
-// const { google} =require('googleapi');
-
-// const CLIENT_ID = '135106361763-tvdu3746d328ri07plfaidvohesberg9.apps.googleusercontent.com'
-// const CLEINT_SECRET='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'
-
-// const REDIRECT_URL='https://developers.google.com/oauthplayground'
-// const REFRESH_TOKEN='1//04RQ-L-yKlDDGCgYIARAAGAQSNwF-L9IrBFMtDsro0Vc_SW_LdKiyXaEVKQPqtrSxkE1JdBpLEFf4wroSTfNlwWlj-2QUCiGeBVs'
-// const oAuth2Client =new google.auth.OAuth2(CLIENT_ID,CLEINT_SECRET,REDIRECT_URL)
-// oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
-
-//  async function sendMail(){
-// try{
-//  const accessToken =await Auth2Client.getAccessToken()
-//  const transport =nodemailer.createTransport({
-//      service:'gmail',
-//      auth: {
-//          type:'OAuth2',
-//          user:'ad1855411@gmail.com',
-//          clientId:CLIENT_ID,
-//          clientSecret:CLEINT_SECRET,
-//          refreshToken:REFRESH_TOKEN,
-//          accessToken:accessToken,
-//      }
-//  })
-//  const mailOptions={
-//      from:'ad1855411@gmail.com ',
-//      to:'s43307123@gmail.com',
-//      subject:"Nodemmailer Project",
-//      text:'Hi from your nodemailer project',
-//      html:'<h1>Hello from your nodemailer Project'
-
-//  };
-//  const result= await transport.sendMail(mailOptions)
-//  return result
-// }
-// catch(error){
-
-// }
-//  }
-//  sendMail()
-//  .then((result)=>console.log("Email sent Successfully",result))
-//  .catch((error)=>console.log(error.message));
-
 const app=express();
+const nodemailer=require('nodemailer');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const dotenv=require('dotenv');
+dotenv.config();
 
+const { google } = require("googleapis");
+const req = require('express/lib/request');
+const OAuth2 = google.auth.OAuth2;
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID  , 
+  process.env.CLIENT_SECRET , 
+  process.env.REDIRECT_URL 
+);
+oauth2Client.setCredentials({
+  refresh_token:process.env.REFRESH_TOKEN 
+});
+const accessToken = oauth2Client.getAccessToken()
+    let transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken:accessToken
+      }
+    });
     const nav= [
         
         
         {
             link:'/home',name:'Email'
         }
-        // ,
-        // {
-        //     link:'/signup',name:'Sign Up'
-        // }
+        
     ]
     
 
-//  const homeRouter=require('./src/routes/homeRoutes')(nav)
+
  
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('./public'));
 app.set('view engine','ejs');
 app.set('views','./src/views');//either . or __dirname 
 
-
-  // app.use('/home',homeRouter);
 
 
 
@@ -88,33 +69,29 @@ app.get('/home',function(req,res){
           });
     });
     app.post('/mailer',function(req,res){
-        res.send("Mail sent Successfully");
+        console.log(req.body.email);
+        var mailOptions = {
+            from: process.env.MAIL_USERNAME,
+            to: req.body.email,
+            subject: 'Sending Email using Node.js',
+            text: 'Hello from nodemailer project .Your given email is Successfully verified!'
+          };
+          transport.sendMail(mailOptions, function(err, data) {
+            if (err) {
+              console.log("Error " + err);
+              res.send("Something went wrong")
+            } else {
+              console.log("Email sent successfully");
+              res.send("Email sent successfully")
+            }
+          });
+
+
+
+
         });
     
         
-        // const transporter = nodemailer.createTransport({
-        //     service:'gmail',
-        //     auth:{
-        //         user:"ad1855411@gmail.com"
-        //     }
-
-        // });
-        
-        // const options ={
-        //     from:"ad1855411@gmail.com",
-        //     to:"s43307123@gmail.com",
-        //     subject:"sending email with node.js",
-        //     text:"sent successufully "
-        
-        // };
-        // transporter.sendMail(options,function(err,info){
-        //     if(err){
-        //         console.log(err);
-        //         return;
-        //     }
-        //     console.log("sent:"+info.response);
-        
-        // })
         
 
 app.listen(process.env.PORT || 9000);
